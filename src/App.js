99d283cf98389bb67f83963065aae60959c20b8f
sindu <sindu0225@gmail.com>
2017-06-28 01:59:21 +0200

    Add fuctionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      content: '/*Add jsx code*/',
+      output:'',
+      err:''
+    }
+  }
+  update(e){
+    try {
+      this.setState({
+        output: window.Babel
+                .transform(e.target.value, {presets: ['es2015','react']})
+                .code,
+        err: ''
+      })
+    }
+    catch(err){
+      this.setState({err: err.message})
+    }
+  }
   render() {
     return (
-        <h1>header</h1>
+      <div className="container-fluid" >
+        <h1 className="text-center">JSX live compiler</h1>
+        <div className='row'>
+          <div className="col-xs-12 col-md-6">
+            <h3>Input</h3>
+            <textarea id="input" className='col-xs-12 col-md-6' 
+                                defaultValue={this.state.content}
+                                onChange={this.update.bind(this)}>
+            </textarea>
+          </div>
+          <div className='col-xs-12 col-md-6'>
+            <h3>output</h3>
+            <div id="output">{this.state.output}</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
